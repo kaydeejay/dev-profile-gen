dc069ef498e29c48f3a356773625f478415489ef
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-26 08:38:09 -0500

    adds inquirer questions and axios call

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 const fs = require('fs');
 const axios = require ('axios');
 const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',  
+      message: 'Enter Your Github Username',
+      name: 'username'
+    },
+    {
+      type: 'list',  
+      message: 'What is your Favorite Color?',
+      choices: ['red','orange','yellow','green','blue','purple'],
+      name: 'favColor'
+    }
+  ])
+  .then(function(response){
+    // console.log(response);
+    const queryURL = `https://api.github.com/users/${response.username}/repos?per_page=100`;
+    axios
+      .get(queryURL)
+      .then(function(response){
+          // do whatever, got the data
+          console.log(response[0]);
+      });
+  }); 
