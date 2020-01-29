fe79888950b25070a2a1d3b8e0302975838eb708
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-29 18:43:18 -0500

    re-factoring to use promises

--- index.js
+++ index.js
@@ -3,37 +3,89 @@ const axios = require ('axios');
 const inquirer = require('inquirer');
 const template = require('./generateHTML.js');
 
-inquirer
-  .prompt([
-    {
-      type: 'input',  
-      message: 'Enter Your Github Username',
-      name: 'username'
-    },
-    {
-      type: 'list',  
-      message: 'What is your Favorite Color?',
-      choices: ['red','pink','green','blue'],
-      name: 'favColor'
-    }
-  ])
-  .then(function(response){
-    const queryURL = `https://api.github.com/users/${response.username}`;
-    const chosenColor = response.favColor;
-    axios
-      .get(queryURL)
-      .then(function(response){
-        response.color = chosenColor;
-        const starsURL = queryURL + `/starred`;
-        axios
-        .get(starsURL)
-        .then(function(starResponse){
-          response.starredLength = starResponse.data.length;
-          const data = template.generateHTML(response);
-          fs.writeFile('resume.html', data, (err) => {
-            if (err) throw err;
-            else console.log('success');
-          });         
+// inquirer
+//   .prompt([
+//     {
+//       type: 'input',  
+//       message: 'Enter Your Github Username',
+//       name: 'username'
+//     },
+//     {
+//       type: 'list',  
+//       message: 'What is your Favorite Color?',
+//       choices: ['red','pink','green','blue'],
+//       name: 'favColor'
+//     }
+//   ])
+//   .then(function(response){
+//     const queryURL = `https://api.github.com/users/${response.username}`;
+//     const chosenColor = response.favColor;
+  //   axios
+  //     .get(queryURL)
+  //     .then(function(response){
+  //       response.color = chosenColor;
+  //       const starsURL = queryURL + `/starred`;
+  //       axios
+  //       .get(starsURL)
+  //       .then(function(starResponse){
+  //         response.starredLength = starResponse.data.length;
+  //         const data = template.generateHTML(response);
+  //         fs.writeFile('resume.html', data, (err) => {
+  //           if (err) throw err;
+  //           else console.log('success');
+  //         });         
+  //       });
+  //     });
+  // });
+
+function getUserResponse(){
+  inquirer
+    .prompt([
+      {
+        type: 'input',  
+        message: 'Enter Your Github Username',
+        name: 'username'
+      },
+      {
+        type: 'list',  
+        message: 'What is your Favorite Color?',
+        choices: ['red','pink','green','blue'],
+        name: 'favColor'
+      }
+    ])
+    .then(function(response){
+      githubCall(response)
+    });
+}
+
+function githubCall(obj){
+  return new Promise((res, rej) => {
+    if (!obj.username || !obj.favColor) {
+      rej("Something went wrong. Please try again");
+    } else {
+      const queryURL = `https://api.github.com/users/${obj.username}`;
+      const chosenColor = obj.favColor;
+      axios
+        .get(queryURL)
+        .then(function(response){
+          response.color = chosenColor;
+          response.starsURL = queryURL + `/starred`;
+          // console.log(response);
+          res(response);
         });
-      });
-  }); 
+    }
+  })
+    .then(funcResponse => githubStarsCall(funcResponse));
+}
+
+function githubStarsCall(obj){
+  return new Promise((res,rej) => {
+    if (!obj.starsURL){
+      rej("Invalid github stars API URL");
+    } else {
+      // do more stuff!
+    }
+  });
+}
+
+getUserResponse();
