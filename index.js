1d006a682cc13b77dea112e64e9e145c136f718e
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-01-29 22:36:12 -0500

    finishes promise chain

--- index.js
+++ index.js
@@ -2,41 +2,10 @@ const fs = require('fs');
 const axios = require ('axios');
 const inquirer = require('inquirer');
 const template = require('./generateHTML.js');
-
-// inquirer
-//   .prompt([
-//     {
-//       type: 'input',  
-//       message: 'Enter Your Github Username',
-//       name: 'username'
-//     },
-//     {
-//       type: 'list',  
-//       message: 'What is your Favorite Color?',
-//       choices: ['red','pink','green','blue'],
-//       name: 'favColor'
-//     }
-//   ])
-//   .then(function(response){
-//     const queryURL = `https://api.github.com/users/${response.username}`;
-//     const chosenColor = response.favColor;
-  //   axios
-  //     .get(queryURL)
-  //     .then(function(response){
-  //       response.color = chosenColor;
-  //       const starsURL = queryURL + `/starred`;
-  //       axios
-  //       .get(starsURL)
-  //       .then(function(starResponse){
-  //         response.starredLength = starResponse.data.length;
-  //         const data = template.generateHTML(response);
-  //         fs.writeFile('resume.html', data, (err) => {
-  //           if (err) throw err;
-  //           else console.log('success');
-  //         });         
-  //       });
-  //     });
-  // });
+const convertFactory = require('electron-html-to');
+const conversion = convertFactory({
+  converterPath: convertFactory.converters.PDF
+});
 
 function getUserResponse(){
   inquirer
@@ -75,7 +44,9 @@ function githubCall(obj){
         });
     }
   })
-    .then(funcResponse => githubStarsCall(funcResponse));
+    .then(funcResponse => githubStarsCall(funcResponse))
+    .then(starResponse => generateHTML(starResponse))
+    .then(rawHTML => convertToPDF(rawHTML));
 }
 
 function githubStarsCall(obj){
@@ -83,7 +54,45 @@ function githubStarsCall(obj){
     if (!obj.starsURL){
       rej("Invalid github stars API URL");
     } else {
-      // do more stuff!
+      // console.log(obj);
+      axios
+      .get(obj.starsURL)
+      .then(function(starResponse){
+        obj.starredLength = starResponse.data.length;
+        res(obj);
+      })
+    }
+  });
+}
+
+function generateHTML(obj){
+  return new Promise((res,rej) => {
+    if (!obj){
+      rej("Where'd your object go?")
+    } else {
+      const data = template.generateHTML(obj);
+      // fs.writeFile('resume.html', data, (err) => {
+      //   if (err) throw err;
+      //   else console.log('success');
+      // });
+      console.log(typeof data);
+      res(data);
+    }
+  });
+}
+
+function convertToPDF(str){
+  return new Promise((res,rej) => {
+    if (!str) {
+      rej('So close...')
+    } else {
+      conversion({ html: str }, function(err, result){
+        if (err) {
+          return console.error(err);
+        }
+        result.stream.pipe(fs.createWriteStream('./resume.pdf'));
+        res('success');
+      });
     }
   });
 }
